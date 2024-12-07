6eeea41d31d8e18a055f6f45f8ceccc40271185b
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-06 17:32:31 -0700

    Create a CustomerOrderCommand using Dapper and UDDT

---
+++ CustomerOrderViewer2.0/Repository/CustomerOrderCommand.cs
@@ -0,0 +1,65 @@
+﻿using CustomerOrderViewer2._0.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerOrderViewer2._0.Repository
+{
+    internal class CustomerOrderCommand
+    {
+        private string _connectionString;
+        public CustomerOrderCommand(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void Upsert(int customerOrderId, int customerId, int itemId, string userId)
+        {
+            //merge statement
+            var upsertStatement = "CustomerOrderDetail_Upsert";
+
+            //using UDDT
+            var dataTable = new DataTable();
+            dataTable.Columns.Add("CustomerOrderId", typeof(int));
+            dataTable.Columns.Add("CustomerId", typeof(int));
+            dataTable.Columns.Add("ItemId", typeof(int));
+            dataTable.Rows.Add(customerOrderId, customerId, itemId);
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Execute(upsertStatement, new { @CustomerOrderType = dataTable.AsTableValuedParameter("CustomerOrderType"), @UserId = userId }, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public void Delete(int customerOrderId, string userId)
+        {
+            var upsertStatement = "CustomerOrderDetail_Delete";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Execute(upsertStatement, new { @CustomerOrderId = customerOrderId, @UserId = userId }, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public IList<CustomerOrderDetailModel> GetList()
+        {
+            List<CustomerOrderDetailModel> customerOrderDetails = new List<CustomerOrderDetailModel>();
+
+            //GetList SPROC (stored procedure)
+            var sql = "CustomerOrderDetail_GetList";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                //using dapper to take care of commands and mapping - Query() is a method from dapper
+                customerOrderDetails = connection.Query<CustomerOrderDetailModel>(sql).ToList();
+            }
+
+            return customerOrderDetails;
+        }
+    }
+}
